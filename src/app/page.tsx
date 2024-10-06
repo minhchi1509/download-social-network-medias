a1e6e482132b4580d8b0a4fb5ae8f180ac876d9e
Nguyễn Minh Chí <minhchico300kc@gmail.com>
2024-10-06 14:20:57 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,86 @@
-import Image from 'next/image';
+'use client';
+import {
+  Box,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text
+} from '@chakra-ui/react';
+
+import {
+  FacebookLogo,
+  InstagramLogo,
+  TiktokLogo,
+  YoutubeLogo
+} from 'src/assets/icons';
+import DouyinDownloader from 'src/components/ui/features/media-downloader/DouyinDownloader';
+import FacebookDownloader from 'src/components/ui/features/media-downloader/FacebookDownloader';
+import InstagramDownloader from 'src/components/ui/features/media-downloader/InstagramDownloader';
+import TiktokDownloader from 'src/components/ui/features/media-downloader/TiktokDownloader';
+import YoutubeDownloader from 'src/components/ui/features/media-downloader/YoutubeDownloader';
+
+const tabsConfig = [
+  {
+    label: 'Instagram',
+    logo: InstagramLogo,
+    component: <InstagramDownloader />
+  },
+  {
+    label: 'Facebook',
+    logo: FacebookLogo,
+    component: <FacebookDownloader />
+  },
+  {
+    label: 'Douyin',
+    logo: TiktokLogo,
+    component: <DouyinDownloader />
+  },
+  {
+    label: 'Tiktok',
+    logo: TiktokLogo,
+    component: <TiktokDownloader />
+  },
+  {
+    label: 'Youtube',
+    logo: YoutubeLogo,
+    component: <YoutubeDownloader />
+  }
+];
 
 export default function Home() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-sm sm:text-left">
-          <li className="mb-2">
-            Get started by editing{' '}
-            <code className="rounded bg-black/[.05] px-1 py-0.5 font-semibold dark:bg-white/[.06]">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex flex-col items-center gap-4 sm:flex-row">
-          <a
-            className="flex h-10 items-center justify-center gap-2 rounded-full border border-solid border-transparent bg-foreground px-4 text-sm text-background transition-colors hover:bg-[#383838] sm:h-12 sm:px-5 sm:text-base dark:hover:bg-[#ccc]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="flex h-10 items-center justify-center rounded-full border border-solid border-black/[.08] px-4 text-sm transition-colors hover:border-transparent hover:bg-[#f2f2f2] sm:h-12 sm:min-w-44 sm:px-5 sm:text-base dark:border-white/[.145] dark:hover:bg-[#1a1a1a]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex flex-wrap items-center justify-center gap-6">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <Box
+      sx={{
+        minHeight: '100vh',
+        width: '100%',
+        maxWidth: '1200px',
+        margin: 'auto',
+        paddingTop: 10,
+        paddingLeft: 30,
+        paddingRight: 30,
+        paddingBottom: 30
+      }}
+    >
+      <Text textAlign="center" fontSize="5xl" fontWeight="extrabold">
+        Trình tải xuống ảnh, video
+      </Text>
+      <Tabs isFitted variant="soft-rounded" colorScheme="gray" mt={2}>
+        <TabList>
+          {tabsConfig.map(({ label, logo: Logo }, index) => (
+            <Tab key={index} display="flex" gap={3} alignItems="center">
+              <Logo width={20} height={20} />
+              {label}
+            </Tab>
+          ))}
+        </TabList>
+        <TabPanels>
+          {tabsConfig.map(({ component }, index) => (
+            <TabPanel key={index}>{component}</TabPanel>
+          ))}
+        </TabPanels>
+      </Tabs>
+    </Box>
   );
 }
---
+++ src/assets/icons/index.ts
@@ -0,0 +1,7 @@
+import FacebookLogo from './facebook-logo.svg';
+import InstagramLogo from './instagram-logo.svg';
+import TickIcon from './tick-icon.svg';
+import TiktokLogo from './tiktok-logo.svg';
+import YoutubeLogo from './youtube-logo.svg';
+
+export { FacebookLogo, InstagramLogo, TickIcon, TiktokLogo, YoutubeLogo };
